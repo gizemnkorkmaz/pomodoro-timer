018f38390f6c00f9d9b1d9dc8bc7cc6f14c76397
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-01-25 13:40:25 +0300

    Change seconds and minutes values && Add minutes state to timer function && Add conditional rendering for seconds

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from "react";
 import Button from "./Button";
 
 function Timer() {
-  const [seconds, setSeconds] = useState(59);
-  const [minutes, setMinutes] = useState(24);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(25);
   const [isTimerActive, setIsTimerActive] = useState(false);
 
   const startTimer = () => setIsTimerActive(true);
@@ -14,16 +14,21 @@ function Timer() {
     const timer = setInterval(() => {
       clearInterval(timer);
 
-      if (isTimerActive && seconds > 0) {
-        setSeconds(seconds - 1);
+      if (isTimerActive) {
+        if (seconds > 0 && minutes >= 0) {
+          setSeconds(seconds - 1);
+        } else if (seconds === 0 && minutes > 0) {
+          setSeconds(59);
+          setMinutes(minutes - 1);
+        }
       }
     }, 1000);
-  }, [isTimerActive, seconds]);
+  }, [isTimerActive, seconds, minutes]);
 
   return (
     <>
       <div>
-        {minutes}:{seconds}
+        {minutes}:{seconds < 10 ? "0" + seconds : seconds}
       </div>
       <Button onClick={startTimer}>Start</Button>
       <Button onClick={pauseTimer}>Pause</Button>
