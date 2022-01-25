ba555f3f32dcaa53b738e6eb5c925db10c1ec9a2
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-01-25 10:56:04 +0300

    Add Timer component

---
+++ src/components/Timer.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+import Button from "./Button";
+
+function Timer() {
+  const [seconds, setSeconds] = useState(59);
+  const [minutes, setMinutes] = useState(24);
+  const [isTimerActive, setIsTimerActive] = useState(false);
+
+  const startTimer = () => setIsTimerActive(true);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      clearInterval(timer);
+
+      if (isTimerActive && seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+    }, 1000);
+  }, [isTimerActive, seconds]);
+
+  return (
+    <>
+      <div>
+        {minutes}:{seconds}
+      </div>
+      <Button onClick={startTimer}>Start</Button>
+    </>
+  );
+}
+
+export default Timer;
