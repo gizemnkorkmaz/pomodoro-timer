ba3333c2e715154ce458b78aa51eb7abe480eb37
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-01-28 19:41:51 +0300

    Add selectRound function && Refactor skipButton function && Add SelectRound component

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
 import Button from "./Button";
-import Rounds from "./Rounds";
+import RoundCount from "./RoundCount";
+import SelectRound from "./SelectRound";
 
 import useInterval from "../hooks/useInterval";
 
@@ -10,31 +11,44 @@ import minutesToSeconds from "../utils/minutesToSeconds";
 
 function Timer() {
   const [seconds, setSeconds] = useState(minutesToSeconds(25));
-  const [roundCount, setRoundCount] = useState(1);
   const [isTimerActive, setIsTimerActive] = useState(false);
-  const [isPomodoro, setIsPomodoro] = useState(true);
-  const [isBreakTime, setIsBreakTime] = useState(false);
+  const [round, setRound] = useState("pomodoro");
+  const [roundCount, setRoundCount] = useState(1);
+
+  const selectRound = (round) => {
+    const roundTime = {
+      pomodoro: 25,
+      longBreak: 15,
+      shortBreak: 5,
+    };
+
+    const roundTimeInSeconds = minutesToSeconds(roundTime[round]);
+
+    setRound(round);
+    setSeconds(roundTimeInSeconds);
+    setIsTimerActive(false);
+  };
 
   const pauseTimer = () => setIsTimerActive(false);
   const startTimer = () => setIsTimerActive(true);
 
   const skipRound = () => {
-    setIsTimerActive(false);
+    const isConfirm = window.confirm(
+      "Are you sure you want to finish the round early?"
+    );
 
-    alert("Are you sure you want to finish the round early?");
-
-    setIsPomodoro(!isPomodoro);
-    setIsBreakTime(!isBreakTime);
-
-    if (isPomodoro) {
-      setRoundCount(roundCount + 1);
-      if (roundCount % 4 === 0) {
-        setSeconds(minutesToSeconds(15));
+    if (isConfirm) {
+      setIsTimerActive(false);
+      if (round === "pomodoro") {
+        if (roundCount % 4 !== 0) {
+          selectRound("shortBreak");
+        } else {
+          selectRound("longBreak");
+        }
       } else {
-        setSeconds(minutesToSeconds(5));
+        selectRound("pomodoro");
+        setRoundCount(roundCount + 1);
       }
-    } else {
-      setSeconds(minutesToSeconds(25));
     }
   };
 
@@ -53,12 +67,13 @@ function Timer() {
 
   return (
     <>
+      <SelectRound selectRound={selectRound} />
       <div>{formattedTime}</div>
       <Button onClick={isTimerActive ? pauseTimer : startTimer}>
         {isTimerActive ? "Pause" : "Start"}
       </Button>
       <Button onClick={skipRound}>Skip</Button>
-      <Rounds roundCount={roundCount} />
+      <RoundCount roundCount={roundCount} />
     </>
   );
 }
