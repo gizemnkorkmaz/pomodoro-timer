6853ee29fd4faec71edaba4e73ce489251a2373d
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-01-29 15:12:18 +0300

    Add styles to Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import styles from "./Header.module.css";
+function Header() {
+  return <h1 className={styles.Header}>Pomodoro Timer</h1>;
+}
+
+export default Header;
