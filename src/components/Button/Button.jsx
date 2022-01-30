7dfa4ddd6c168c52977dcd5cb5516e44d8e60cf6
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-01-30 22:41:26 +0300

    Add isButtonActive prop && class condition to Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 import styles from "./Button.module.css";
 
-function Button({ children, ...props }) {
+function Button({ children, isButtonActive, ...props }) {
   return (
-    <button className={styles.Button} {...props}>
+    <button
+      className={`${styles.Button} ${isButtonActive ? styles.active : ""}`}
+      {...props}
+    >
       {children}
     </button>
   );
