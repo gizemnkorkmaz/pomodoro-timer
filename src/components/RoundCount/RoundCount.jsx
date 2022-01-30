79968b4d5f907ed4ae7340b9701770c164a0433c
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-01-30 16:21:06 +0300

    Add round expression to RoundCount

--- src/components/RoundCount/RoundCount.jsx
+++ src/components/RoundCount/RoundCount.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./RoundCount.module.css";
 
 function RoundCount({ roundCount }) {
-  return <div className={styles.RoundCount}>#{roundCount}</div>;
+  return <div className={styles.RoundCount}>Round {roundCount}</div>;
 }
 
 export default RoundCount;
