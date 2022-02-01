67dd34b88778efe1c490885624b686870a0a17c5
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-02-01 21:26:46 +0300

    Add setCustomTimer function && Add custom timer input and save button to CustomTimerModal

--- src/components/CustomTimerModal/CustomTimerModal.jsx
+++ src/components/CustomTimerModal/CustomTimerModal.jsx
@@ -3,14 +3,46 @@ import React from "react";
 import Button from "../Button/Button";
 import Modal from "../Modal/Modal";
 
-function CustomTimerModal({ isOpen, setIsOpen, isShowCloseButton }) {
+import minutesToSeconds from "../../utils/minutesToSeconds";
+
+import styles from "./CustomTimerModal.module.css";
+
+function CustomTimerModal({
+  isOpen,
+  isShowCloseButton,
+  setIsOpen,
+  setSeconds,
+  setIsTimerActive,
+  setRound,
+}) {
+  const setCustomTimer = (event) => {
+    setIsTimerActive(false);
+    const customTime = minutesToSeconds(event.target.value);
+    setSeconds(customTime);
+    setRound("pomodoro");
+  };
+
   return (
     <Modal
       isOpen={isOpen}
       setIsOpen={setIsOpen}
       isShowCloseButton={isShowCloseButton}
     >
-      <Button onClick={() => setIsOpen(false)}>Save</Button>
+      <div className={styles.CustomTimerContainer}>
+        <h2>Set your own focus time:</h2>
+        <label className={styles.InputLabel}>
+          Time (in minutes)
+          <input
+            type="number"
+            placeholder="e.g. 25"
+            onChange={(event) => setCustomTimer(event)}
+            className={styles.InputArea}
+          />
+        </label>
+      </div>
+      <Button className={styles.SaveButton} onClick={() => setIsOpen(false)}>
+        Save
+      </Button>
     </Modal>
   );
 }
