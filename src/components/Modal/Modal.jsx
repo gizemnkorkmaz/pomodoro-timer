f2798dcaa0a0306ff83648a14ad845c2dd8aa4d1
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-02-01 15:43:59 +0300

    Add modal component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Button from "../Button/Button";
+
+import { ReactComponent as CloseIcon } from "../../assets/icons/CloseIcon.svg";
+import styles from "./Modal.module.css";
+
+function Modal({ children, isOpen, setIsOpen, isShowCloseButton }) {
+  const closeModal = () => {
+    setIsOpen(isOpen);
+  };
+
+  return (
+    isOpen && (
+      <div className={styles.Modal}>
+        {children}
+        {isShowCloseButton && (
+          <Button className={styles.CloseButton} onClick={closeModal}>
+            <CloseIcon />
+          </Button>
+        )}
+      </div>
+    )
+  );
+}
+
+export default Modal;
