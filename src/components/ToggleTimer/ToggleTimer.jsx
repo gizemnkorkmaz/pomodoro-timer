d9ddd48d242fb765ba6f20efbd5b3597da3db541
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-02-05 13:56:52 +0300

    Add ToggleTimer component

---
+++ src/components/ToggleTimer/ToggleTimer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { ReactComponent as SettingsIcon } from "../../assets/icons/SettingsIcon.svg";
+import { ReactComponent as CloseIcon } from "../../assets/icons/CloseIcon.svg";
+
+import styles from "./ToggleTimer.module.css";
+
+import Button from "../Button/Button";
+
+function ToggleTimer({
+  isTimerActive,
+  isOpenCustomTimer,
+  setIsOpenCustomTimer,
+}) {
+  return (
+    <Button
+      className={`${isTimerActive ? styles.SettingsIcon : styles.CloseIcon}`}
+      onClick={() => setIsOpenCustomTimer(!isOpenCustomTimer)}
+    >
+      {isOpenCustomTimer ? <CloseIcon /> : <SettingsIcon />}
+    </Button>
+  );
+}
+
+export default ToggleTimer;
