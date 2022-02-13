413145a7da419dcea809466b987131293f02ace2
Aykut Kardaş <aykutkrds@gmail.com>
2022-02-13 22:28:28 +0300

    Refactor ToggleTimerButton

--- src/components/ToggleTimerButton/ToggleTimerButton.jsx
+++ src/components/ToggleTimerButton/ToggleTimerButton.jsx
@@ -1,32 +1,16 @@
-import React from "react";
-
 import { ReactComponent as SettingsIcon } from "../../assets/icons/SettingsIcon.svg";
 import { ReactComponent as CloseIcon } from "../../assets/icons/CloseIcon.svg";
 
 import styles from "./ToggleTimerButton.module.css";
 
-import Button from "../Button/Button";
-
-function ToggleTimerButton({
-  isTimerActive,
-  isOpenCustomTimer,
-  setIsOpenCustomTimer,
-  activeRound,
-}) {
-  return (
-    <div
-      className={`${
-        activeRound === "pomodoro" ? styles.ShowButton : styles.HideButton
-      }`}
-    >
-      <Button
-        className={`${isTimerActive ? styles.SettingsIcon : styles.CloseIcon}`}
-        onClick={() => setIsOpenCustomTimer(!isOpenCustomTimer)}
-      >
-        {isOpenCustomTimer ? <CloseIcon /> : <SettingsIcon />}
-      </Button>
-    </div>
-  );
-}
+const ToggleTimerButton = ({ isOpenCustomTimer, setIsOpenCustomTimer }) => (
+  <span
+    role="button"
+    className={styles.SettingsIcon}
+    onClick={() => setIsOpenCustomTimer(!isOpenCustomTimer)}
+  >
+    {isOpenCustomTimer ? <CloseIcon /> : <SettingsIcon />}
+  </span>
+);
 
 export default ToggleTimerButton;
