816d9a821dac507118bc5493f2cd2717d7bbd42f
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-02-14 01:53:00 +0300

    Change function declaration to expression in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,5 @@
-import React from "react";
-
 import styles from "./Header.module.css";
 
-function Header() {
-  return <h1 className={styles.Header}>Pomodoro Timer</h1>;
-}
+const Header = () => <h1 className={styles.Header}>Pomodoro Timer</h1>;
 
 export default Header;
