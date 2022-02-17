dc19309ef4a2792ee7bf0c2eaf1dbff5fb0bf1ce
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-02-17 18:15:31 +0300

    Update formatTime

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -4,6 +4,8 @@ import duration from "dayjs/plugin/duration";
 dayjs.extend(duration);
 
 const formatTime = (seconds) =>
-  dayjs.duration(seconds, "seconds").format("mm:ss");
+  dayjs
+    .duration({ seconds: seconds % 60, minutes: Math.floor(seconds / 60) })
+    .format("mm:ss");
 
 export default formatTime;
